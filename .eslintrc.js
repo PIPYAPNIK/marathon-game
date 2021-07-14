a3deddcbd3da1ec4688b2756f9a2add2efb29c63
kirill_m <kirill_m@maximail.org>
2021-07-14 13:39:32 +0500

    homework-3 --moved player and draw world

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,12 @@ module.exports = {
     'guard-for-in': 'off',
     'no-restricted-syntax': 'off',
     'object-curly-newline': 'off',
+    'no-unused-expressions': 'off',
+    'no-plusplus': 'off',
+    'camelcase': 'off',
+    'no-unused-vars': 'off',
+    'no-irregular-whitespace': 'off',
+    'no-param-reassign': 'off',
+    'import/prefer-default-export': 'off',
   },
 };
--- src/client/ClientGame.js
+++ src/client/ClientGame.js
@@ -3,16 +3,23 @@ import ClientWorld from './ClientWorld';
 
 import sprites from '../configs/sprites';
 import levelCfg from '../configs/world.json';
+import gameObjects from '../configs/gameObjects.json';
 
 class ClientGame {
   constructor(cfg) {
     Object.assign(this, {
       cfg,
+      gameObjects,
+      player: null,
     });
 
     this.engine = this.createEngine();
     this.world = this.createWorld();
     this.initEngine();
+  }
+
+  setPlayer(player) {
+    this.player = player;
   }
 
   createEngine() {
@@ -25,11 +32,28 @@ class ClientGame {
 
   initEngine() {
     this.engine.loadSprites(sprites).then(() => {
-      this.engine.on('render', () => {
-        this.world.init();
+      this.world.init();
+      this.engine.on('render', (_, time) => {
+        this.world.render(time);
       });
       this.engine.start();
+      this.initKeys();
     });
+  }
+
+  initKeys() {
+    this.engine.input.onKey({
+      ArrowLeft: (keyDown) => this.playerMove(keyDown, -1, 0),
+      ArrowRight: (keyDown) => this.playerMove(keyDown, 1, 0),
+      ArrowUp: (keyDown) => this.playerMove(keyDown, 0, -1),
+      ArrowDown: (keyDown) => this.playerMove(keyDown, 0, 1),
+    });
+  }
+
+  playerMove(keyDown, dcol, drow) {
+    if (keyDown) {
+      this.player.moveByCellCoord(dcol, drow, (cell) => cell.findObjectsByType('grass').length);
+    }
   }
 
   static init(cfg) {
